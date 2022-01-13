dd7b7314044635f1f3a21c7ed5363675dc4b3ec0
tamal15 <tamal15-13243@diu.edu.bd>
2022-01-13 11:05:59 +0600

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const admin = require("firebase-admin");
 const { MongoClient } = require('mongodb');
-const ObjectId=require('mongodb').ObjectId
+const ObjectId=require('mongodb').ObjectId;
+const { v4: uuidv4 } = require('uuid');
+// const SSLCommerzPayment = require('sslcommerz')
 require('dotenv').config()
 //  payment 
 
@@ -13,6 +15,8 @@ const cors = require('cors')
 
 const app = express()
 const port =process.env.PORT || 5000;
+const SSLCommerzPayment = require('sslcommerz')
+app.use(express.urlencoded({ extended: true }));
 
 // doctor-portal-firebase-adminsdk-fb393-1696fcd38a.json 
 
@@ -58,6 +62,7 @@ async function run(){
       const database=client.db('doctor_portal')
       const appointmentCollection=database.collection('appointment')
       const registerCollection=database.collection('register')
+      const othersPaymentCollection=database.collection('othersPayment')
      
       // app get 
       app.get('/appointments', async(req,res)=>{
@@ -156,6 +161,118 @@ async function run(){
       })
 
 
+      // bikash payment start 
+
+      //sslcommerz init
+app.post('/init', async(req, res) => {
+  // console.log(req.body)
+  const data = {
+      total_amount: req.body.total_amount,
+      currency: 'BDT',
+      tran_id: uuidv4(),
+      success_url: 'http://localhost:5000/success',
+      fail_url: 'http://localhost:5000/fail',
+      cancel_url: 'http://localhost:5000/cancel',
+      ipn_url: 'http://localhost:5000/ipn',
+      shipping_method: 'Courier',
+      paymentStatus:'panding',
+      product_name: req.body.product_name,
+      product_category: 'Electronic',
+      product_profile: 'general',
+      cus_name: req.body.cus_name,
+      cus_email: req.body.cus_email,
+      cus_add1: 'Dhaka',
+      cus_add2: 'Dhaka',
+      cus_city: 'Dhaka',
+      cus_state: 'Dhaka',
+      cus_postcode: '1000',
+      cus_country: 'Bangladesh',
+      cus_phone: '01711111111',
+      cus_fax: '01711111111',
+      ship_name: 'Customer Name',
+      ship_add1: 'Dhaka',
+      ship_add2: 'Dhaka',
+      ship_city: 'Dhaka',
+      ship_state: 'Dhaka',
+      ship_postcode: 1000,
+      ship_country: 'Bangladesh',
+      multi_card_name: 'mastercard',
+      value_a: 'ref001_A',
+      value_b: 'ref002_B',
+      value_c: 'ref003_C',
+      value_d: 'ref004_D'
+  };
+  // console.log(data)
+  // insert data into database 
+  const order=await othersPaymentCollection.insertOne(data)
+  const sslcommer = new SSLCommerzPayment(process.env.STORE_ID, process.env.STORE_PASSWORD,false) //true for live default false for sandbox
+  sslcommer.init(data).then(data => {
+      //process the response that got from sslcommerz 
+      //https://developer.sslcommerz.com/doc/v4/#returned-parameters
+      // console.log(data)
+      if(data.GatewayPageURL){
+        res.json(data.GatewayPageURL)
+      }
+      else{
+        return res.status(400).json({
+          message:'payment session failed'
+        })
+      }
+     
+  });
+})
+
+app.post ('/success', async(req,res)=>{
+  // console.log(req.body);
+  const order = await othersPaymentCollection.updateOne({tran_id:req.body.tran_id},{
+    $set:{
+      val_id:req.body.val_id
+    }
+
+  })
+  res.status(200).redirect(`http://localhost:3000/success/${req.body.tran_id}`)
+})
+app.post ('/fail', async(req,res)=>{
+  // console.log(req.body);
+const order=await othersPaymentCollection.deleteOne({tran_id:req.body.tran_id})
+  res.status(400).redirect(`http://localhost:3000`)
+})
+app.post ('/cancel', async(req,res)=>{
+  // console.log(req.body);
+  const order=await othersPaymentCollection.deleteOne({tran_id:req.body.tran_id})
+  res.status(200).redirect(`http://localhost:3000`)
+})
+
+// payment validate check and status update for pading to confarm 
+app.post('/validate', async(req,res)=>{
+  console.log(req.body)
+  const order =await othersPaymentCollection.findOne({tran_id:req.body.tran_id});
+  console.log(order)
+  if(order.val_id === req.body.val_id){
+    const update = await othersPaymentCollection.updateOne({tran_id:req.body.tran_id},{
+      $set:{
+        paymentStatus:'paid'
+      }
+    })
+    res.send(update.modifiedCount>0)
+  }
+  else{
+    res.send('payment not confirmed. appointment discarded')
+  }
+})
+
+app.get('/orders/:tran_id', async(req,res)=>{
+  const id=req.params.tran_id;
+  const order =await othersPaymentCollection.findOne({tran_id:id});
+  console.log(order)
+  res.json(order)
+})
+
+
+
+      // bikash payment end 
+
+
       //  payment method type post 
        app.post('/create-payment-intent',async(req,res)=>{
          const paymentInfo=req.body;
@@ -170,6 +287,14 @@ async function run(){
        })
       
     }
+
+
+    // bikash payment start
+
+    
+
+
+    // bikash payment end 
     finally{
     //    await client.close();
     }
